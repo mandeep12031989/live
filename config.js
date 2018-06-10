0be1911ed18d6ff7b93e9204349d506d3e2b8d97
Avinash Sharma <ravinashkvafs@gmail.com>
2018-06-10 12:44:50 +0530

    TRANSFER

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ module.exports = {
     "development":{
         'secretKey': '7HE91-QM30X-ZYKS5-8G8HA-BCW0J-PDN2C',
         'scrt': 'portal\/\/\'*iD',
-        'mongoUrl' : 'mongodb://lavish:idiscoverdb@ds129143.mlab.com:29143/testing-new',
+        'mongoUrl' : 'mongodb://idiscover:idiscover123@ds229458.mlab.com:29458/app',
         'googleAuth' : {
             'clientID'      : '437438716282-4s1rmctjev4njem80m4a12j5fg47u850.apps.googleusercontent.com',
             'clientSecret'  : 'tPkCriCx-Q7hvS8DwauyutOT',
@@ -14,7 +14,7 @@ module.exports = {
     "production":{
         'secretKey': '7HE91-QM30X-ZYKS5-8G8HA-BCW0J-PDN2C',
         'scrt': 'portal\/\/\'*iD',
-        'mongoUrl' : 'mongodb://lavish:idiscoverdb@ds129143.mlab.com:29143/testing-new',
+        'mongoUrl' : 'mongodb://idiscover:idiscover123@ds229458.mlab.com:29458/app',
         'googleAuth' : {
             'clientID'      : '437438716282-4s1rmctjev4njem80m4a12j5fg47u850.apps.googleusercontent.com',
             'clientSecret'  : 'tPkCriCx-Q7hvS8DwauyutOT',
